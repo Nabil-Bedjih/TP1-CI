d9555de8add34b8353521248d933a9dd2772e7fb
Aymerick772 <Aymerick772@gmail.com>
2024-11-05 10:13:06 +0100

    commit

--- src/main/java/fr/epsi/Service.java
+++ src/main/java/fr/epsi/Service.java
@@ -5,7 +5,7 @@ import static fr.epsi.GameCharacterController.characters;
 @org.springframework.stereotype.Service
 public class Service {
     public void additem(){
-        characters.add(new GameCharacter("Gandalf", "Mage"));
+        characters.add(new GameCharacter("Gandalfs", "Mage"));
         characters.add(new GameCharacter("Legolas", "Archer"));
         characters.add(new GameCharacter("Aragorn", "Guerrier"));
         characters.add(new GameCharacter("Frodon", "Hobbit"));
