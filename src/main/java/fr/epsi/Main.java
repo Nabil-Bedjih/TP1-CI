b1f9121b1ce35d2cf3732f412a04137f0860184c
Aymerick772 <Aymerick772@gmail.com>
2024-11-05 10:19:36 +0100

    commit test

--- src/main/java/fr/epsi/Main.java
+++ src/main/java/fr/epsi/Main.java
@@ -6,7 +6,7 @@ public class Main {
         GameCharacter archer = new GameCharacter("Legolas", "Archer");
 
         Quest easyQuest = new Quest("Sauver le village", 1, 2);
-        Quest hardQuest = new Quest("Tuer le dragon", 5, 5);
+        Quest hardQuest = new Quest("Tuer le dragon mechant", 5, 5);
 
         System.out.println(mage);
         System.out.println(archer);
